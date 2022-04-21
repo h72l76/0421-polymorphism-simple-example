4593771035f2102248c6ccccdefcb0b54f390d45
Huabo Lu <huabolu@outlook.com>
2022-04-21 12:13:38 -0500

    add comments to file

--- 0421-polymorphism-simple-example/0421-polymorphism-simple-example.cpp
+++ 0421-polymorphism-simple-example/0421-polymorphism-simple-example.cpp
@@ -5,21 +5,47 @@
 
 class Base {
 public: 
-	void func() const { std::cout << "hello from base. \n"; }
+	// use virtual in base class function to support polymorphism
+	virtual void func() const { std::cout << "hello from base. \n"; }
+
+	// define a virtual dtor 
+	virtual ~Base() = default; 
+
 };
 
 class Derived : public Base {
 public:
-	void func() const { std::cout << "hello from derived. \n"; }
+	// (optional) use override keyword
+	void func() const override { std::cout << "hello from derived. \n"; }
 };
+
+// use pass-by-reference to support polymorphism
+void helloFunc(const Base& b) { b.func();  }
 
 int main()
 {
 	Base b; 
 	Derived d; 
 
+	// using pointers (dynamic binding): polymorphism
+	std::cout << "polymorphism by using pointer: \n";
 	Base* pb = &b; 
 	pb->func(); 
+
 	pb = &d; 
 	pb->func(); 
+	std::cout << std::endl;
+
+	// using reference (dynamic binding): polymorphism
+	std::cout << "polymorphism by using reference: \n";
+	helloFunc(b); 
+	helloFunc(d); 
+	std::cout << std::endl;
+
+	// using object (not reference or pointer): no polymorphism
+	std::cout << "no polymorphism if only using object: \n";
+	auto obj = b; 
+	obj.func(); 
+	obj = d; 
+	obj.func(); 
 }
